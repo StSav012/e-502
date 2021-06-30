c8a51ad4dcc5a261a11e8e5de7a68213ef8271db
stsav012 <stsav012@gmail.com>
2021-06-30 18:15:07 +0300

    WIP: Adding a GUI

--- plotter.py
+++ plotter.py
@@ -2,15 +2,15 @@
 import sys
 from multiprocessing import Process, Queue
 from pathlib import Path
-from typing import Dict, List, Optional, Set, Union, Type
+from typing import Dict, Iterable, List, Optional, Set, TextIO, Tuple, Union
 
 import numpy as np
-import pyqtgraph as pg
+import pathvalidate
+import pyqtgraph as pg  # type: ignore
 from PySide6.QtCore import QSettings, QTimer, Qt, Signal
-from PySide6.QtGui import QCloseEvent, QValidator, QColor
-from PySide6.QtWidgets import QApplication, QFormLayout, QGroupBox, QHBoxLayout, QLineEdit, \
-    QMainWindow, \
-    QPushButton, QSpinBox, QTabWidget, QVBoxLayout, QWidget, QSizePolicy
+from PySide6.QtGui import QCloseEvent, QColor, QValidator
+from PySide6.QtWidgets import QApplication, QFileDialog, QFormLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit, \
+    QMainWindow, QPushButton, QSizePolicy, QSpinBox, QStyle, QTabWidget, QVBoxLayout, QWidget
 
 from channel_settings import ChannelSettings
 from e502 import E502, X502_ADC_FREQ_DIV_MAX
@@ -21,20 +21,32 @@ except ImportError:
     # stub
     class _Final:
         @staticmethod
-        def __getitem__(item: Type) -> Type:
+        def __getitem__(item: type) -> type:
             return item
 
 
     Final = _Final()
 
+try:
+    from typing import Literal
+except ImportError:
+    from typing import Any
+    # stub
+    class _Literal:
+        @staticmethod
+        def __getitem__(*items: Any) -> type:
+            return type(items[0]) if items else Any
+
+
+    Literal = _Literal()
+
 
 class Measurement(Process):
-    def __init__(self, requests_queue: Queue, results_queue: Queue,
+    def __init__(self, results_queue: Queue[np.ndarray],
                  ip_address: str, settings: List[ChannelSettings], adc_frequency_divider: int,
                  data_portion_size: int) -> None:
         super(Measurement, self).__init__()
-        self.requests_queue: Queue = requests_queue
-        self.results_queue: Queue = results_queue
+        self.results_queue: Queue[np.ndarray] = results_queue
 
         self.device: E502 = E502(ip_address)
         self.device.write_channels_settings_table(settings)
@@ -74,7 +86,7 @@ class IPAddressValidator(QValidator):
 
 
 class FilePathEntry(QWidget):
-    def __init__(self, parent: Optional[QWidget] = None):
+    def __init__(self, initial_file_path: str = '', parent: Optional[QWidget] = None):
         super().__init__(parent)
 
         self.path: Optional[Path] = None
@@ -82,20 +94,57 @@ class FilePathEntry(QWidget):
         self.setLayout(QHBoxLayout())
 
         self.text: QLineEdit = QLineEdit(self)
-        # TODO: on the text change,
-        #  • validate the entered path and
-        #  • indicate somehow if it is invalid
+        self.text.setText(initial_file_path)
+        self.text.textChanged.connect(self.on_text_changed)
         self.layout().addWidget(self.text)
 
+        self.status: QLabel = QLabel(self)
+        self.layout().addWidget(self.status)
+
         self.browse_button: QPushButton = QPushButton('Browse…', self)
-        # TODO: on the button click,
-        #  • open a file dialog,
-        #  • store the result in `path` variable,
-        #  • display the result in the text box,
-        #  • set the text box tooltip to the box content in case the box is too small for the path
+        self.browse_button.clicked.connect(self.on_browse_button_clicked)
         self.layout().addWidget(self.browse_button)
 
         self.layout().setStretch(1, 0)
+        self.layout().setStretch(2, 0)
+
+    def on_text_changed(self, text: str) -> None:
+        """ display an icon showing whether the entered file name is acceptable """
+
+        self.text.setToolTip(text)
+
+        if not text:
+            self.status.clear()
+            self.path = None
+            return
+
+        path: Path = Path(text).resolve()
+        if path.is_dir():
+            self.status.setPixmap(self.style().standardIcon(QStyle.SP_MessageBoxCritical).pixmap(self.text.height()))
+            self.path = None
+            return
+        if path.exists():
+            self.status.setPixmap(self.style().standardIcon(QStyle.SP_MessageBoxWarning).pixmap(self.text.height()))
+            self.path = path
+            return
+        try:
+            pathvalidate.validate_filepath(path, platform='auto')
+        except pathvalidate.error.ValidationError as ex:
+            print(ex.description)
+            self.status.setPixmap(self.style().standardIcon(QStyle.SP_MessageBoxCritical).pixmap(self.text.height()))
+            self.path = None
+        else:
+            self.status.clear()
+            self.path = path
+
+    def on_browse_button_clicked(self) -> None:
+        new_file_name: str
+        new_file_name, _ = QFileDialog.getSaveFileName(
+            self, 'Save As...',
+            str(self.path or ''),
+            'Tab Separated Values (*.tsv)')
+        if new_file_name:
+            self.text.setText(new_file_name)
 
 
 class ChannelSettingsGUI(QGroupBox, ChannelSettings):
@@ -174,6 +223,8 @@ class ChannelSettingsGUI(QGroupBox, ChannelSettings):
             pass
         self.color_button.sigColorChanged.connect(self.on_color_changed)
 
+        self.saving_location: FilePathEntry = FilePathEntry(self.settings.value('savingLocation', '', str), self)
+
         self.setLayout(QFormLayout())
         layout: QFormLayout = self.layout()
         layout.addRow('Range:', self.combo_range)
@@ -181,7 +232,7 @@ class ChannelSettingsGUI(QGroupBox, ChannelSettings):
         layout.addRow('Mode:', self.combo_mode)
         layout.addRow('Averaging:', self.spin_averaging)
         layout.addRow('Line Color:', self.color_button)
-        layout.addRow('Data File:', FilePathEntry(self))
+        layout.addRow('Data File:', self.saving_location)
         r: int
         for r in range(layout.rowCount()):
             layout.itemAt(r, QFormLayout.LabelRole).widget().setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -241,7 +292,7 @@ class ChannelSettingsGUI(QGroupBox, ChannelSettings):
 
 
 class GUI(QMainWindow):
-    def __init__(self, flags=Qt.WindowFlags()) -> None:
+    def __init__(self, flags: Qt.WindowFlags = Qt.WindowFlags()) -> None:
         super(GUI, self).__init__(flags=flags)
 
         self.settings: QSettings = QSettings('SavSoft', 'E-502', self)
@@ -263,11 +314,11 @@ class GUI(QMainWindow):
         self.spin_portion_size: QSpinBox = QSpinBox(self.parameters_box)
         self.spin_frequency_divider: QSpinBox = QSpinBox(self.parameters_box)
 
-        self.tabs: QTabWidget = QTabWidget(self.central_widget)
-        self.tab: List[ChannelSettingsGUI] = [ChannelSettingsGUI(self.settings) for _ in range(8)]
+        self.tabs_container: QTabWidget = QTabWidget(self.central_widget)
+        self.tabs: List[ChannelSettingsGUI] = [ChannelSettingsGUI(self.settings) for _ in range(8)]
         t: ChannelSettingsGUI
-        for i, t in enumerate(self.tab):
-            t.color_button.setColor(pg.intColor(i, hues=len(self.tab)))
+        for i, t in enumerate(self.tabs):
+            t.color_button.setColor(pg.intColor(i, hues=len(self.tabs)))
 
         self.button_start: QPushButton = QPushButton(self.central_widget)
         self.button_stop: QPushButton = QPushButton(self.central_widget)
@@ -277,7 +328,7 @@ class GUI(QMainWindow):
         self.setup_actions()
 
     def setup_ui_appearance(self) -> None:
-        opts: Dict[str, Union[bool, str, int]]
+        opts: Dict[str, Union[bool, str, int, float, Tuple[float, float]]]
         opts = {
             'suffix': 'S/s',
             'siPrefix': True,
@@ -311,7 +362,7 @@ class GUI(QMainWindow):
         self.main_layout.addWidget(self.plot)
         self.main_layout.addLayout(self.controls_layout)
         self.controls_layout.addWidget(self.parameters_box)
-        self.controls_layout.addWidget(self.tabs)
+        self.controls_layout.addWidget(self.tabs_container)
         self.controls_layout.addLayout(self.buttons_layout)
 
         self.parameters_layout.addRow('IP Address:', self.text_ip_address)
@@ -322,7 +373,7 @@ class GUI(QMainWindow):
 
         index: int
         for index in range(8):
-            self.tabs.addTab(self.tab[index], str(index + 1))
+            self.tabs_container.addTab(self.tabs[index], str(index + 1))
 
         self.buttons_layout.addWidget(self.button_start)
         self.buttons_layout.addWidget(self.button_stop)
@@ -333,7 +384,7 @@ class GUI(QMainWindow):
 
         self.setCentralWidget(self.central_widget)
 
-        any_channel_active: bool = any(t.isChecked() for t in self.tab)
+        any_channel_active: bool = any(t.isChecked() for t in self.tabs)
         self.button_start.setEnabled(any_channel_active)
 
     def setup_actions(self) -> None:
@@ -342,9 +393,9 @@ class GUI(QMainWindow):
 
         index: int
         for index in range(8):
-            self.tab[index].channelChanged.connect(self.on_tab_channel_changed)
-            self.tab[index].toggled.connect(self.on_tab_toggled)
-            self.tab[index].colorChanged.connect(self.on_tab_color_changed)
+            self.tabs[index].channelChanged.connect(self.on_tab_channel_changed)
+            self.tabs[index].toggled.connect(self.on_tab_toggled)
+            self.tabs[index].colorChanged.connect(self.on_tab_color_changed)
 
     def load_settings(self) -> None:
         self.restoreGeometry(self.settings.value('windowGeometry', b''))
@@ -379,77 +430,127 @@ class GUI(QMainWindow):
     def on_button_start_clicked(self) -> None:
         self.button_start.setDisabled(True)
         self.parameters_box.setDisabled(True)
-        self.tabs.setDisabled(True)
+        self.tabs_container.setDisabled(True)
         self.button_stop.setEnabled(True)
 
     def on_button_stop_clicked(self) -> None:
         self.button_stop.setDisabled(True)
         self.parameters_box.setEnabled(True)
-        self.tabs.setEnabled(True)
+        self.tabs_container.setEnabled(True)
         self.button_start.setEnabled(True)
 
     def on_tab_channel_changed(self, channel: int) -> None:
         index: int
         tab: ChannelSettingsGUI
-        tab_index: int = self.tabs.indexOf(self.sender())
-        for index, tab in enumerate(self.tab):
+        tab_index: int = self.tabs_container.indexOf(self.sender())
+        for index, tab in enumerate(self.tabs):
             if index == tab_index:
                 continue
             if channel == tab.physical_channel:
                 tab.setChecked(False)
 
     def on_tab_toggled(self, on: bool) -> None:
-        any_channel_active: bool = any(t.isChecked() for t in self.tab)
+        any_channel_active: bool = any(t.isChecked() for t in self.tabs)
         self.button_start.setEnabled(any_channel_active)
         self.button_stop.setDisabled(any_channel_active)
         if not on:
             return
         other_channels: Set[int] = set()
         tab: ChannelSettingsGUI
-        for tab in self.tab:
-            if tab.isChecked() and tab is not self.sender():
+        for tab in self.tabs:
+            if tab.isChecked() and tab is not self.sender() and tab.physical_channel is not None:
                 other_channels.add(tab.physical_channel)
         self.sender().spin_channel.setValue(min(set(list(range(self.sender().spin_channel.minimum() - 1,
                                                                self.sender().spin_channel.maximum())))
                                                 .difference(other_channels)) + 1)
 
     def on_tab_color_changed(self, color: QColor) -> None:
-        for line, tab in zip(self.plot_lines, self.tab):
+        for line, tab in zip(self.plot_lines, self.tabs):
             if self.sender() is tab:
                 line.setPen(color)
 
 
+class FileWriter(Process):
+    def __init__(self, requests_queue: Queue):
+        super(Process, self).__init__()
+
+        self.requests_queue: Queue[Tuple[Path,
+                                         Literal['w', 'w+', '+w', 'wt', 'tw', 'wt+', 'w+t', '+wt', 'tw+', 't+w', '+tw',
+                                                 'a', 'a+', '+a', 'at', 'ta', 'at+', 'a+t', '+at', 'ta+', 't+a', '+ta',
+                                                 'x', 'x+', '+x', 'xt', 'tx', 'xt+', 'x+t', '+xt', 'tx+', 't+x', '+tx'],
+                                         np.ndarray]] = requests_queue
+
+    @property
+    def done(self) -> bool:
+        return self.requests_queue.empty()
+
+    def run(self) -> None:
+        file_path: Path
+        file_mode: Literal['w', 'w+', '+w', 'wt', 'tw', 'wt+', 'w+t', '+wt', 'tw+', 't+w', '+tw',
+                           'a', 'a+', '+a', 'at', 'ta', 'at+', 'a+t', '+at', 'ta+', 't+a', '+ta',
+                           'x', 'x+', '+x', 'xt', 'tx', 'xt+', 'x+t', '+xt', 'tx+', 't+x', '+tx']
+        x: np.ndarray
+        f_out: TextIO
+
+        while True:
+            file_path, file_mode, x = self.requests_queue.get(block=True)
+            with file_path.open(file_mode) as f_out:
+                f_out.writelines((('\t'.join(f'{xii}' for xii in xi)
+                                   if isinstance(xi, Iterable)
+                                   else f'{xi}'
+                                   ) + '\n')
+                                 for xi in x)
+
+
 class App(GUI):
-    def __init__(self, flags=Qt.WindowFlags()) -> None:
+    def __init__(self, flags: Qt.WindowFlags = Qt.WindowFlags()) -> None:
         super(App, self).__init__(flags=flags)
 
         self.timer: QTimer = QTimer(self)
         self.timer.timeout.connect(self.on_timeout)
 
-        self.requests_queue: Queue = Queue()
-        self.results_queue: Queue = Queue()
+        self.requests_queue: Queue[Tuple[Path,
+                                         Literal['w', 'w+', '+w', 'wt', 'tw', 'wt+', 'w+t', '+wt', 'tw+', 't+w', '+tw',
+                                                 'a', 'a+', '+a', 'at', 'ta', 'at+', 'a+t', '+at', 'ta+', 't+a', '+ta',
+                                                 'x', 'x+', '+x', 'xt', 'tx', 'xt+', 'x+t', '+xt', 'tx+', 't+x', '+tx'],
+                                         np.ndarray]] = Queue()
+        self.results_queue: Queue[np.ndarray] = Queue()
         self.measurement: Optional[Measurement] = None
+        self.file_writer: Optional[FileWriter] = None
+
+        self._index_map: List[int] = []
 
     def on_button_start_clicked(self) -> None:
         super(App, self).on_button_start_clicked()
-        active_settings: List[ChannelSettingsGUI] = [t for t in self.tab if t.isChecked()]
+        t: ChannelSettingsGUI
+        i: int
+        self._index_map = []
+        for i, t in enumerate(self.tabs):
+            if t.isChecked():
+                self._index_map.append(i)
+        active_settings: List[ChannelSettingsGUI] = [t for t in self.tabs if t.isChecked()]
         self.canvas.clearPlots()
-        self.plot_lines = [self.canvas.plot([], [],
+        self.plot_lines = [self.canvas.plot(np.empty(0),
                                             name=f'{t.physical_channel}',
                                             pen=t.color_button.color())
                            for t in active_settings]
         self.timer.start(10)
-        self.measurement = Measurement(self.requests_queue, self.results_queue,
+        self.measurement = Measurement(self.results_queue,
                                        ip_address=self.text_ip_address.text(),
                                        settings=active_settings,
                                        adc_frequency_divider=self.spin_frequency_divider.value(),
                                        data_portion_size=self.spin_portion_size.value())
         self.measurement.start()
+        self.file_writer = FileWriter(self.requests_queue)
+        self.file_writer.start()
 
     def on_button_stop_clicked(self) -> None:
         if self.measurement is not None:
             self.measurement.terminate()
             self.measurement.join()
+        if self.file_writer is not None:
+            self.file_writer.terminate()
+            self.file_writer.join()
         self.timer.stop()
         super(App, self).on_button_stop_clicked()
 
@@ -458,10 +559,10 @@ class App(GUI):
             data: np.ndarray = self.results_queue.get()
             ch: int
             for ch, line in zip(range(data.shape[1]), self.plot_lines):
-                line.setData(data[..., ch])
-                # TODO:
-                #  • display not only one data junk but the whole trend, up to the specified length,
-                #  • store the received data into the specified files (if a path is given)
+                line.setData(np.concatenate((line.yData, data[..., ch])))
+                tab_index: int = self._index_map[ch]
+                if self.tabs[tab_index].saving_location:
+                    self.requests_queue.put((self.tabs[tab_index].saving_location, 'at', data[..., ch]))
 
 
 if __name__ == '__main__':
