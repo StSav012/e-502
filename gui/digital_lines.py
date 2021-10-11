ed3651b39b25496be32756ae972f31e9d94b936a
StSav012 <stsav012@gmail.com>
2021-10-11 23:46:54 +0300

    Digital lines are separate controls now to introduce some meaning in them

--- gui/digital_lines.py
+++ gui/digital_lines.py
@@ -4,10 +4,7 @@ from __future__ import annotations
 
 from typing import List, Optional, Iterator
 
-import pyqtgraph as pg  # type: ignore
-
 from gui.pg_qt import *
-from gui.toggle_button import ToggleButton
 
 __all__ = ['DigitalLines']
 
@@ -18,30 +15,76 @@ class DigitalLines(QGroupBox):
             super().__init__(title, parent)
         else:
             super().__init__(parent)
-        layout: QHBoxLayout = QHBoxLayout(self)
-        self.buttons: List[QPushButton] = []
-        i: int
-        for i in range(16):
-            self.buttons.append(ToggleButton(Qt.GlobalColor.darkGreen, str(i + 1), self))
-            self.buttons[-1].setFixedWidth(self.buttons[-1].minimumSizeHint().height())
-            layout.addWidget(self.buttons[-1])
+        layout: QFormLayout = QFormLayout(self)
+        self.buttons: List[bool] = [False] * 8
+
+        self.setCheckable(True)
+        self.toggled.connect(self.on_toggled)
+
+        self.more_emitter_voltage: QCheckBox = QCheckBox(self.tr('Increase emitter voltage'), self)
+        layout.addWidget(self.more_emitter_voltage)
+        self.more_emitter_voltage.toggled.connect(self.on_more_emitter_voltage_toggled)
+
+        self.combo_amplification: QComboBox = QComboBox(self)
+        self.combo_amplification.addItems(('×1', '×2', '×4', '×8'))
+        layout.addRow(self.tr('Amplification:'), self.combo_amplification)
+        self.combo_amplification.currentIndexChanged.connect(self.on_amplification_changed)
+
+        self.combo_pulse_duration: QComboBox = QComboBox(self)
+        self.combo_pulse_duration.addItems(('5 us', '10 us', '20 us', '40 us'))
+        layout.addRow(self.tr('Pulse duration:'), self.combo_pulse_duration)
+        self.combo_pulse_duration.currentIndexChanged.connect(self.on_pulse_duration_changed)
+
+        self.combo_pulse_rate: QComboBox = QComboBox(self)
+        self.combo_pulse_rate.addItems(('2 Hz', '10 Hz', '40 Hz', '100 Hz'))
+        layout.addRow(self.tr('Pulse rate:'), self.combo_pulse_rate)
+        self.combo_pulse_rate.currentIndexChanged.connect(self.on_pulse_rate_changed)
 
     def __len__(self) -> int:
         return len(self.buttons)
 
     def __getitem__(self, index: int) -> bool:
         if 1 <= index <= len(self):
-            return self.buttons[index - 1].isChecked()
+            return self.buttons[index - 1]
         else:
-            raise IndexError
+            return False
+            # raise IndexError
 
     def __setitem__(self, index: int, pushed: bool) -> None:
-        if 1 <= index <= len(self):
-            return self.buttons[index - 1].setChecked(pushed)
-        else:
-            raise IndexError
+        index -= 1
+        if 0 <= index < len(self):
+            self.buttons[index] = pushed
+
+            if index == 0:
+                self.setChecked(pushed)
+            elif index == 1:
+                self.more_emitter_voltage.setChecked(pushed)
+            elif index in (2, 3):
+                self.combo_amplification.setCurrentIndex(self.buttons[2] + self.buttons[3] * 2)
+            elif index in (4, 5):
+                self.combo_pulse_duration.setCurrentIndex(self.buttons[4] + self.buttons[5] * 2)
+            elif index in (6, 7):
+                self.combo_pulse_rate.setCurrentIndex(self.buttons[6] + self.buttons[7] * 2)
+        # else:
+        #     raise IndexError
 
     def __iter__(self) -> Iterator[bool]:
-        b: QPushButton
-        for b in self.buttons:
-            yield b.isChecked()
+        yield from self.buttons
+
+    def on_toggled(self, on: bool) -> None:
+        self.buttons[0] = on
+
+    def on_more_emitter_voltage_toggled(self, on: bool) -> None:
+        self.buttons[1] = on
+
+    def on_amplification_changed(self, index: int) -> None:
+        self.buttons[2] = bool(index & 1)
+        self.buttons[3] = bool(index & 2)
+
+    def on_pulse_duration_changed(self, index: int) -> None:
+        self.buttons[4] = bool(index & 1)
+        self.buttons[5] = bool(index & 2)
+
+    def on_pulse_rate_changed(self, index: int) -> None:
+        self.buttons[6] = bool(index & 1)
+        self.buttons[7] = bool(index & 2)
--- gui/pg_qt.py
+++ gui/pg_qt.py
@@ -7,22 +7,26 @@ import pyqtgraph as pg
 if pg.Qt.QT_LIB == pg.Qt.PYSIDE6:
     from PySide6.QtCore import QTimer, QSettings, Qt, Signal, QRect, QByteArray, QPoint
     from PySide6.QtWidgets import QGroupBox, QHBoxLayout, QPushButton, QWidget, QFormLayout, QGroupBox, QSizePolicy, \
-        QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget
+        QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget, \
+        QCheckBox, QComboBox
     from PySide6.QtGui import QColor, QCloseEvent, QValidator, QPalette, QPaintEvent
 elif pg.Qt.QT_LIB == pg.Qt.PYQT5:
     from PyQt5.QtCore import QTimer, QSettings, Qt, pyqtSignal as Signal, QRect, QByteArray, QPoint
     from PyQt5.QtWidgets import QGroupBox, QHBoxLayout, QPushButton, QWidget, QFormLayout, QGroupBox, QSizePolicy, \
-        QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget
+        QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget, \
+        QCheckBox, QComboBox
     from PyQt5.QtGui import QCloseEvent, QColor, QPaintEvent, QPalette, QValidator
 elif pg.Qt.QT_LIB == pg.Qt.PYQT6:
     from PyQt6.QtCore import QTimer, QSettings, Qt, pyqtSignal as Signal, QRect, QByteArray, QPoint
     from PyQt6.QtWidgets import QGroupBox, QHBoxLayout, QPushButton, QWidget, QFormLayout, QGroupBox, QSizePolicy, \
-        QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget
+        QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget, \
+        QCheckBox, QComboBox
     from PyQt6.QtGui import QCloseEvent, QColor, QPaintEvent, QPalette, QValidator
 elif pg.Qt.QT_LIB == pg.Qt.PYSIDE2:
     from PySide2.QtCore import QTimer, Qt, QSettings, Signal, QRect, QByteArray, QPoint
     from PySide2.QtWidgets import QGroupBox, QHBoxLayout, QPushButton, QWidget, QFormLayout, QGroupBox, QSizePolicy, \
-        QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget
+        QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget, \
+        QCheckBox, QComboBox
     from PySide2.QtGui import QCloseEvent, QColor, QPaintEvent, QPalette, QValidator
 
     QApplication.exec = QApplication.exec_
@@ -44,7 +48,7 @@ __all__ = [
     'QTabWidget',
     'QFormLayout', 'QHBoxLayout', 'QVBoxLayout',
     'QGroupBox',
-    'QLabel', 'QPushButton', 'QLineEdit', 'QSpinBox',
+    'QLabel', 'QCheckBox', 'QPushButton', 'QLineEdit', 'QSpinBox', 'QComboBox',
     'QFileDialog',
     'QMainWindow',
     'QApplication',
