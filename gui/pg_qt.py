999c4ccce882010f4d91e25a9f1c4e4e4a5f689f
StSav012 <stsav012@gmail.com>
2021-10-16 22:47:43 +0300

    No need to import whole pyqtgraph for Qt version detection

--- gui/pg_qt.py
+++ gui/pg_qt.py
@@ -2,27 +2,27 @@
 
 """ a convenient import of all Qt classes used """
 
-import pyqtgraph as pg
+from pyqtgraph import Qt
 
-if pg.Qt.QT_LIB == pg.Qt.PYSIDE6:
+if Qt.QT_LIB == Qt.PYSIDE6:
     from PySide6.QtCore import QTimer, QSettings, Qt, Signal, QRect, QByteArray, QPoint, QModelIndex
     from PySide6.QtWidgets import QGroupBox, QHBoxLayout, QPushButton, QWidget, QFormLayout, QGroupBox, QSizePolicy, \
         QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget, \
         QCheckBox, QComboBox, QToolButton, QDialog, QListWidget
     from PySide6.QtGui import QColor, QCloseEvent, QValidator, QPalette, QPaintEvent
-elif pg.Qt.QT_LIB == pg.Qt.PYQT5:
+elif Qt.QT_LIB == Qt.PYQT5:
     from PyQt5.QtCore import QTimer, QSettings, Qt, pyqtSignal as Signal, QRect, QByteArray, QPoint, QModelIndex
     from PyQt5.QtWidgets import QGroupBox, QHBoxLayout, QPushButton, QWidget, QFormLayout, QGroupBox, QSizePolicy, \
         QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget, \
         QCheckBox, QComboBox, QToolButton, QDialog, QListWidget
     from PyQt5.QtGui import QCloseEvent, QColor, QPaintEvent, QPalette, QValidator
-elif pg.Qt.QT_LIB == pg.Qt.PYQT6:
+elif Qt.QT_LIB == Qt.PYQT6:
     from PyQt6.QtCore import QTimer, QSettings, Qt, pyqtSignal as Signal, QRect, QByteArray, QPoint, QModelIndex
     from PyQt6.QtWidgets import QGroupBox, QHBoxLayout, QPushButton, QWidget, QFormLayout, QGroupBox, QSizePolicy, \
         QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget, \
         QCheckBox, QComboBox, QToolButton, QDialog, QListWidget
     from PyQt6.QtGui import QCloseEvent, QColor, QPaintEvent, QPalette, QValidator
-elif pg.Qt.QT_LIB == pg.Qt.PYSIDE2:
+elif Qt.QT_LIB == Qt.PYSIDE2:
     from PySide2.QtCore import QTimer, Qt, QSettings, Signal, QRect, QByteArray, QPoint, QModelIndex
     from PySide2.QtWidgets import QGroupBox, QHBoxLayout, QPushButton, QWidget, QFormLayout, QGroupBox, QSizePolicy, \
         QSpinBox, QLineEdit, QApplication, QLabel, QStyle, QFileDialog, QMainWindow, QVBoxLayout, QTabWidget, \
@@ -32,7 +32,7 @@ elif pg.Qt.QT_LIB == pg.Qt.PYSIDE2:
     QApplication.exec = QApplication.exec_
 else:
     raise ImportError('PySide6, or PyQt5, or PySide2, is required. PyQt6 support is not guaranteed. '
-                      f'{pg.Qt.QT_LIB} is not supported at all.')  # in case pg add support for another framework
+                      f'{Qt.QT_LIB} is not supported at all.')  # in case pg add support for another framework
 
 __all__ = [
     'Qt',
