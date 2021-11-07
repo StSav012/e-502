f6a4805d6589fb313a0b25426ccacd77181f764d
StSav012 <stsav012@gmail.com>
2021-11-07 20:02:39 +0300

    Initial commit

---
+++ gui/dir_path_entry.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Optional
+
+import pathvalidate
+
+from gui.pg_qt import *
+
+__all__ = ['DirPathEntry']
+
+
+class DirPathEntry(QWidget):
+    def __init__(self, initial_file_path: str = '', parent: Optional[QWidget] = None):
+        super().__init__(parent)
+
+        self.path: Optional[Path] = None
+
+        self.setLayout(QHBoxLayout())
+
+        self.text: QLineEdit = QLineEdit(self)
+        self.text.setText(initial_file_path)
+        self.text.textChanged.connect(self.on_text_changed)
+        self.layout().addWidget(self.text)
+
+        self.status: QLabel = QLabel(self)
+        self.layout().addWidget(self.status)
+
+        self.browse_button: QPushButton = QPushButton('Browse…', self)
+        self.browse_button.clicked.connect(self.on_browse_button_clicked)
+        self.layout().addWidget(self.browse_button)
+
+        self.layout().setStretch(1, 0)
+        self.layout().setStretch(2, 0)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+
+    def on_text_changed(self, text: str) -> None:
+        """ display an icon showing whether the entered file name is acceptable """
+
+        self.text.setToolTip(text)
+
+        if not text:
+            self.status.clear()
+            self.path = None
+            return
+
+        path: Path = Path(text).resolve()
+        if not path.is_dir():
+            self.status.setPixmap(self.style().standardIcon(QStyle.SP_MessageBoxCritical).pixmap(self.text.height()))
+            self.path = None
+            return
+        if not path.exists():
+            self.status.setPixmap(self.style().standardIcon(QStyle.SP_MessageBoxWarning).pixmap(self.text.height()))
+            self.path = path
+            return
+        try:
+            pathvalidate.validate_filepath(path, platform='auto')
+        except pathvalidate.error.ValidationError as ex:
+            print(ex.description)
+            self.status.setPixmap(self.style().standardIcon(QStyle.SP_MessageBoxCritical).pixmap(self.text.height()))
+            self.path = None
+        else:
+            self.status.clear()
+            self.path = path
+
+    def on_browse_button_clicked(self) -> None:
+        new_dir_name: str = QFileDialog.getExistingDirectory(
+            self, 'Save To...',
+            str(self.path or ''),
+            QFileDialog.Option.ShowDirsOnly | QFileDialog.Option.DontUseNativeDialog)
+        if new_dir_name:
+            self.text.setText(new_dir_name)
